9c670dd5e75f7c3840ac6328eb7fe066a439ebc2
Martin Skalický <skalicky.martin@iotdomu.cz>
2026-01-10 23:09:58 +0100

    feat: allow to skip expensive check, keep monthly backup

--- script_sync.sh
+++ script_sync.sh
@@ -9,9 +9,6 @@ fi
 # Clean up old backups: keep 7 end of day and 4 additional end of week archives.
 # Prune operation is not important, s3 sync is - do not exit were this to fail
 echo "Validating repository integrity and compacting..."
-borg check
-borg prune -v --list --keep-daily=7 --keep-weekly=4
-borg compact
 
 SYNC_COMMAND="aws s3 sync ${BORG_REPO} s3://${BORG_S3_BACKUP_BUCKET} --delete --exclude 'lock.exclusive/*'"
 
